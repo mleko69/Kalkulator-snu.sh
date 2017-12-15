a51c513f0c0cc6954ce38a4b7558970d1edb3fc1
mleko69 <p.andryszczuk@gmail.com>
2017-12-15 12:45:34 +0100

    Update kalkulator-snu.sh

--- kalkulator-snu.sh
+++ kalkulator-snu.sh
@@ -9,14 +9,29 @@ echo "2) Chcę dowiedzieć się o której wstać."
 printf "Twój wybór: "
 read tryb
 
+echo #pusta przestrzeń dla estetyki
+echo "Zanim zaczniemy. Jak szybko zasypiasz? (w minutach)"
+echo "1) Ok 15 minut. (Domyślne)"
+echo "2) Trochę dłużej/krócej. (Własne)"
+printf "Twój wybór: "
+read TrybZasypiania
+
 function print1 {
+  echo #pusta przestrzeń dla estetyki
   printf "Ok, o której chcesz wstać? "
   read godzina
 }
 
 function print2 {
+ echo  #pusta przestrzeń dla estetyki
  printf "Ok, o której chcesz się położyć? "
  read godzina
+}
+
+function IleZasypia {
+  echo  #pusta przestrzeń dla estetyki
+  printf "Ok, ile minut zasypiasz? "
+  read CzasZasypiania
 }
 
 function obliczenia {
@@ -93,17 +108,103 @@ echo " $wynik5" "(Pięć cykli: 7,5 godziny snu)"
 echo #pusta przestrzeń dla estetyki
 }
 
+function obliczeniaCust {
+#Dane
+zmienna="$godzina"
+zmiennaZZZ="$CzasZasypiania" #czas na zaśnięcie
+nzmienna="01:30"
+nzmienna2="03:00"
+nzmienna3="04:30"
+nzmienna4="06:00"
+nzmienna5="07:30"
+
+#Skrypt konwertuje powyższe stałe i zmienną na sekundy (potrzebne do obliczeń):
+PodanaGodzina=$(date -u -d "$zmienna" +"%s")
+PodanaZZZ=$(date -u -d "$zmiennaZZZ" +"%s")
+Const=$(date -u -d "$nzmienna" +"%s")
+Const2=$(date -u -d "$nzmienna2" +"%s")
+Const3=$(date -u -d "$nzmienna3" +"%s")
+Const4=$(date -u -d "$nzmienna4" +"%s")
+Const5=$(date -u -d "$nzmienna5" +"%s")
+
+#Operacje na przekonwertowanych danych:
+wynik=$(date -u -d "0 $PodanaGodzina sec - $Const sec - $PodanaZZZ sec " +"%H:%M")
+wynik2=$(date -u -d "0 $PodanaGodzina sec - $Const2 sec - $PodanaZZZ sec " +"%H:%M")
+wynik3=$(date -u -d "0 $PodanaGodzina sec - $Const3 sec - $PodanaZZZ sec " +"%H:%M")
+wynik4=$(date -u -d "0 $PodanaGodzina sec - $Const4 sec - $PodanaZZZ sec " +"%H:%M")
+wynik5=$(date -u -d "0 $PodanaGodzina sec - $Const5 sec - $PodanaZZZ sec " +"%H:%M")
+
+#Output kalkulacji:
+echo #pusta przestrzeń dla estetyki
+echo "Powinieneś/aś się położyć o tych godzinach (zakładając że potrzebujesz ok. "$CzasZasypiania" minut na zaśnięcie):"
+echo " $wynik" "(Jeden cykl: 1,5 godziny snu)"
+echo " $wynik2" "(Dwa cykle: 3 godziny snu)"
+echo " $wynik3" "(Trzy cykle: 4,5 godziny snu)"
+echo " $wynik4" "(Cztery cykle: 6 godzin snu)"
+echo " $wynik5" "(Pięć cykli: 7,5 godziny snu)"
+echo #pusta przestrzeń dla estetyki
+}
+
+function obliczeniaCust2 {
+#Dane
+zmienna="$godzina"
+zmiennaZZZ="$CzasZasypiania" #czas na zaśnięcie
+nzmienna="01:30"
+nzmienna2="03:00"
+nzmienna3="04:30"
+nzmienna4="06:00"
+nzmienna5="07:30"
+
+#Skrypt konwertuje powyższe stałe i zmienną na sekundy (potrzebne do obliczeń):
+PodanaGodzina=$(date -u -d "$zmienna" +"%s")
+PodanaZZZ=$(date -u -d "$zmiennaZZZ" +"%s")
+Const=$(date -u -d "$nzmienna" +"%s")
+Const2=$(date -u -d "$nzmienna2" +"%s")
+Const3=$(date -u -d "$nzmienna3" +"%s")
+Const4=$(date -u -d "$nzmienna4" +"%s")
+Const5=$(date -u -d "$nzmienna5" +"%s")
+
+#Operacje na przekonwertowanych danych:
+wynik=$(date -u -d "0 $PodanaGodzina sec + $Const sec + $PodanaZZZ sec " +"%H:%M")
+wynik2=$(date -u -d "0 $PodanaGodzina sec + $Const2 sec + $PodanaZZZ sec " +"%H:%M")
+wynik3=$(date -u -d "0 $PodanaGodzina sec + $Const3 sec + $PodanaZZZ sec " +"%H:%M")
+wynik4=$(date -u -d "0 $PodanaGodzina sec + $Const4 sec + $PodanaZZZ sec " +"%H:%M")
+wynik5=$(date -u -d "0 $PodanaGodzina sec + $Const5 sec + $PodanaZZZ sec " +"%H:%M")
+
+#Output kalkulacji:
+echo #pusta przestrzeń dla estetyki
+echo "Powinieneś/aś wstać o tych godzinach (zakładając że potrzebujesz ok. "$CzasZasypiania" minut na zaśnięcie):"
+echo " $wynik" "(Jeden cykl: 1,5 godziny snu)"
+echo " $wynik2" "(Dwa cykle: 3 godziny snu)"
+echo " $wynik3" "(Trzy cykle: 4,5 godziny snu)"
+echo " $wynik4" "(Cztery cykle: 6 godzin snu)"
+echo " $wynik5" "(Pięć cykli: 7,5 godziny snu)"
+echo #pusta przestrzeń dla estetyki
+}
+
 #Funkcje if:
-if [ "$tryb" -eq "1" ] ; then
-  print1 && obliczenia
-elif [ "$tryb" -eq "1" ] ; then
-  : obliczenia2 #zapobiega wykonywaniu działań z funkcji obliczenia2
+if [ "$tryb" -eq "1" -a "$TrybZasypiania" = "1" ] ; then
+print1 && obliczenia
+elif [ "$tryb" -eq "1" -a "$TrybZasypiania" = "1" ] ; then
+  : obliczenia2 obliczeniaCust obliczeniaCust2 IleZasypia #zapobiega wykonywaniu niepotrzebnych działań
 fi
 
-if [ "$tryb" -eq "2" ] ; then
-  print2 && obliczenia2
-elif [ "$tryb" -eq "2" ] ; then
-  : obliczenia #zapobiega wykonywaniu działań z funkcji obliczenia
+if [ "$tryb" -eq "1" -a "$TrybZasypiania" = "2" ] ; then
+  IleZasypia && print1 && obliczeniaCust
+elif [ "$tryb" -eq "1" -a "$TrybZasypiania" = "1" ] ; then
+  : obliczenia obliczenia2 obliczeniaCust2 #zapobiega wykonywaniu niepotrzebnych działań
+fi
+
+if [ "$tryb" -eq "2" -a "$TrybZasypiania" = "1" ] ; then
+print2 && obliczenia2
+elif [ "$tryb" -eq "1" -a "$TrybZasypiania" = "1" ] ; then
+  : obliczenia obliczeniaCust obliczeniaCust2 IleZasypia #zapobiega wykonywaniu niepotrzebnych działań
+fi
+
+if [ "$tryb" -eq "2" -a "$TrybZasypiania" = "2" ] ; then
+  IleZasypia && print2 && obliczeniaCust2
+elif [ "$tryb" -eq "2" -a "$TrybZasypiania" = "2" ] ; then
+  : obliczenia obliczenia2 obliczeniaCust #zapobiega wykonywaniu niepotrzebnych działań
 fi
 
 exit
